4b05fd4b4e086b9088bc9e53d6e184ec8854bf69
sabrina <sabrinaabrodi20@gmail.com>
2022-09-08 20:40:57 +0200

    importing note list component to app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
+import NotesList from "./components/NotesList";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="container">
+      <NotesList />
+    </div>
+  );
 };
 
 export default App;
