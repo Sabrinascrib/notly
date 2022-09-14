f5acca342f7164d0d7f2d53302a0c02d76884242
sabrina <sabrinaabrodi20@gmail.com>
2022-09-14 19:34:27 +0200

    removing tailwind classes

--- src/components/Note.js
+++ src/components/Note.js
@@ -3,11 +3,11 @@ import { MdDeleteForever } from "react-icons/md";
 
 export const Note = () => {
   return (
-    <div className="note w-96">
+    <div className="note">
       <span>Hello! This is our first note! </span>
 
-      <div className="note-footer bg-slate-600 flex flex-row">
-        <small className="text-red-400">11/11/2022</small>
+      <div className="note-footer">
+        <small className="">11/11/2022</small>
         <MdDeleteForever className="delete-icon" size="1.3em" />
       </div>
     </div>
