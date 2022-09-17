c25ff9e84e26a96db609da55deccd2c44383b18a
sabrina <sabrinaabrodi20@gmail.com>
2022-09-17 19:17:29 +0200

    note object

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { nanoid } from "nanoid";
 import { NotesList } from "./components/NotesList";
 
 export const App = () => {
+  const [notes, setNotes] = useState([
+    {
+      id: nanoid(),
+      text: "This is my first note!",
+      date: "15/04/2021",
+    },
+    {
+      id: nanoid(),
+      text: "This is my second note!",
+      date: "21/04/2021",
+    },
+    {
+      id: nanoid(),
+      text: "This is my third note!",
+      date: "28/04/2021",
+    },
+  ]);
   return (
     <div className="container">
-      <NotesList />
+      <NotesList notes={notes} />
     </div>
   );
 };
